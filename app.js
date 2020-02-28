70cf01ea041c170f7d22d1fdb49594f36f3b00d8
suzuki takahiro <tachisatok@gmail.com>
2020-02-29 00:01:32 +0900

    指数オーダーのfib関数の実装

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
 'use strict';
+
+function fibonacci(num) {
+    if (num === 0) {
+        return 0;
+    } else if (num === 1) {
+        return 1;
+    }
+
+    return fibonacci(num - 1) + fibonacci(num - 2);
+}
+
+const number = 40;
+for (let i = 0; i < number; i++) {
+    console.log(fibonacci(i));
+}
